5bdf6fff230a44183d87bde67cbfecf8105c0e88
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-10-08 22:05:22 -0500

    change config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://fyncaluque.github.io",
-  base: "/fyncaluque.github.io",
   integrations: [tailwind()],
 });
